9f0f81017bed6a25feaf83271f50cde3d19880d6
Max Bilokur <bilmaxim@gmail.com>
2023-04-21 22:21:36 +0300

    image fields disabled, urls edited

--- social_media/models.py
+++ social_media/models.py
@@ -10,11 +10,11 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=100, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
     bio = models.CharField(max_length=255, null=True, blank=True)
-    profile_picture = models.ImageField(
-        upload_to="profile_pictures",
-        null=True,
-        blank=True
-    )
+    # profile_picture = models.ImageField(
+    #     upload_to="profile_pictures",
+    #     null=True,
+    #     blank=True
+    # )
     follows = models.ManyToManyField(
         to="self",
         blank=True,
@@ -26,11 +26,11 @@ class Profile(models.Model):
         return self.username
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+# class Tag(models.Model):
+#     name = models.CharField(max_length=100, unique=True)
 
-    def __str__(self) -> str:
-        return self.name
+#     def __str__(self) -> str:
+#         return self.name
 
 
 class Post(models.Model):
@@ -41,16 +41,17 @@ class Post(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     body = models.CharField(max_length=255)
-    tags = models.ManyToManyField(
-        to=Tag,
-        blank=True,
-        related_name="tasks"
-    )
-    attachment = models.ImageField(
-        upload_to="post_attachements",
-        null=True,
-        blank=True
-    )
+    tags = models.CharField(max_length=100, null=True, blank=True)
+    # tags = models.ManyToManyField(
+    #     to=Tag,
+    #     blank=True,
+    #     related_name="tasks"
+    # )
+    # attachment = models.ImageField(
+    #     upload_to="post_attachements",
+    #     null=True,
+    #     blank=True
+    # )
 
     def __str__(self) -> str:
         return f"post # {self.id}"
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui"
     ),
+    path("api/social/", include("social_media.urls", namespace="social")),
 ]
